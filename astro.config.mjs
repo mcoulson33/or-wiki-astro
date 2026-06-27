a9176bb412d1e2d3702940c66d79b118e6db15e6
Matt Coulson <47127731+mcoulson33@users.noreply.github.com>
2026-06-27 16:14:55 -0500

    Testing changing formats from md to mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import starlight from '@astrojs/starlight';
 
 import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
     integrations: [
 		starlight({
